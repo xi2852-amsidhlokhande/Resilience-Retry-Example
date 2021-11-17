6362f77b8f8695f83ff4b574f4e99c8c2705db7f
Amsidh.Lokhande <amsidh.lokhande@xebia.com>
2021-11-17 16:14:23 +0530

    Service name added to each microservice

--- MicroService-5/src/main/java/com/amsidh/mvc/controller/Service5Controller.java
+++ MicroService-5/src/main/java/com/amsidh/mvc/controller/Service5Controller.java
@@ -7,10 +7,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.concurrent.ThreadLocalRandom;
 
@@ -24,15 +21,23 @@ public class Service5Controller {
 
     private int requestCount = 0;
 
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @GetMapping("/{service5Id}")
     public ResponseEntity<ResponseService5> getService5ById(@PathVariable("service5Id") Integer service5Id) {
-        log.info("Request No- " + requestCount++);
+        log.info("Request No ==============" + requestCount++);
         // simulate failure
-        int random = ThreadLocalRandom.current().nextInt(1, 4);
+        int random = ThreadLocalRandom.current().nextInt(1, 8);
         log.info("Random Value " + random);
-        if (random < 3) {
-            return ResponseEntity.status(500).build();
-        } else {
+        if (1 <= random && random <= 4) {
+            log.error("Error with 500 status code");
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        } else if (4 <= random && random <= 6) {
+            log.error("Error with 400 status code");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        } else if (6 <= random && random <= 7) {
+            log.error("Error with 408 status code");
+            return ResponseEntity.status(HttpStatus.REQUEST_TIMEOUT).build();
+        }else {
+            log.info("Request success");
             Service5 service5 = service5Service.getService5ById(service5Id);
             return ResponseEntity.ok(ResponseService5.builder()
                     .service5Id(service5.getService5Id())
